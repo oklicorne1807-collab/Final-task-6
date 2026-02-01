df29d93a99c5463687a907bb5ba88d56530ed320
Oksana <oklicorne1807@gmail.com>
2026-02-01 22:48:15 +0300

    6-3

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/Yandex-Practicum/golf1-sprint6-final/internal/server"
+)
+
+func main() {
+	logger := log.New(os.Stdout, "SERVER: ", log.LstdFlags)
+
+	srv := server.New(logger)
+
+	if err := srv.Server.ListenAndServe(); err != nil {
+		logger.Fatal(err)
+	}
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,35 @@
 package server
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Yandex-Practicum/golf1-sprint6-final/internal/handlers"
+)
+
+type Server struct {
+	Logger *log.Logger
+	Server *http.Server
+}
+
+func New(logger *log.Logger) *Server {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", handlers.IndexHandler)
+	mux.HandleFunc("/upload", handlers.UploadHandler)
+
+	httpServer := &http.Server{
+		Addr:         ":8080",
+		Handler:      mux,
+		ErrorLog:     logger,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+
+	return &Server{
+		Logger: logger,
+		Server: httpServer,
+	}
+}
